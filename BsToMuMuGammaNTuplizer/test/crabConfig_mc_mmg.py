44112c865057178f5a9ea550a8fdb7561f6bebf2
rchudasa <rchudasa@cern.ch>
2021-05-24 06:20:21 +0200

    crab config for ntupliser job submission

---
+++ BsToMuMuGammaNTuplizer/test/crabConfig_mc_mmg.py
@@ -0,0 +1,27 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+#config.section_('General')
+config.General.requestName = 'ntuples_mmg'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+#config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'ntuplize_AOD.py'
+config.JobType.maxMemoryMB = 4000
+
+config.Data.inputDataset ='/BsToMuMuGamma_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-evtgen-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM'
+
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+
+#config.Data.outLFNDirBase = '/store/group/phys_diffraction/lbyl_2018/mc_lbl/ntuples'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/rchudasa/lowPT_photonReco'
+config.Data.allowNonValidInputDataset = True
+config.Data.publication = False
+config.Data.outputDatasetTag = 'ntuples_mmg'
+config.Site.storageSite = 'T2_CH_CERN'
