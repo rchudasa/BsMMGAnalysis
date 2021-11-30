0386758db9308893ed85c09b5b8c23939bad9a67
Aravind Sugunan <aravindsugunan@gmail.com>
2021-11-30 15:34:51 +0530

    clean up

--- Analysis/makeSetupAndFillCodeFromBranches.py
+++ Analysis/makeSetupAndFillCodeFromBranches.py
@@ -1,57 +1,56 @@
+#!/usr/bin/env python3
 
-f=open("branches.br.dimu")
+def writeSetupAssignFiles(txt,setupFile,fillFile,nCands,candMax):
+    setupFile.write("\n")
+    setupFile.write("\tauto idx = offset ;\n ")
+    fillFile.write( "\tInt_t idx = 0 ;\n ")
+    
+    for l in txt:
+        brname=l[:-1]
+        setupFile.write("\n")
+        setupFile.write('\tcandidateMap["'+brname+'"] = idx; \n')
+        setupFile.write('\toutTree->Branch("'+brname+'" , &(storageArray[idx] ) , "'+brname+'['+nCands+']/D"); \n')
+       # setupFile.write('\tstd::cout<< "'+brname+'"<<" idx : "<<idx<<"  candidateMap['+brname+'] = "<<candidateMap["'+brname+'"]<<"\\n";\n')
+        setupFile.write('\tidx+='+candMax+'; \n')
+    
+        fillFile.write('\tstorageArray[ offset + candidateMap["'+brname+'"]] = ntupleRawTree.' + brname+ '[candIdx] ; \n')
+        #fillFile.write('\tidx = candidateMap["'+brname+'"] ; \n')
+        #fillFile.write('\tstd::cout<< "'+brname+'"<<" idx : "<<idx<<"  val = "<<storageArray[idx]<<"\\n";\n\n')
+    setupFile.write("\n\treturn idx;")
+
+
+f=open("branches/branches.br.dimu")
 txt=f.readlines()
 f.close()
-
 setupFile=open("setupDimu.cc",'w')
 fillFile =open("assignDimu.cc",'w')
-
-i=0
-setupFile.write("\n")
-setupFile.write("\tauto idx = offset ;\n ")
-fillFile.write( "\tInt_t idx = 0 ;\n ")
-
-for l in txt:
-    brname=l[:-1]
-    setupFile.write("\n")
-    setupFile.write('\tcandidateMap["'+brname+'"] = idx; \n')
-    setupFile.write('\toutTree->Branch("'+brname+'" , &(storageArray[idx] ) ); \n')
-   # setupFile.write('\tstd::cout<< "'+brname+'"<<" idx : "<<idx<<"  candidateMap['+brname+'] = "<<candidateMap["'+brname+'"]<<"\\n";\n')
-    setupFile.write('\tidx++; \n')
-
-    fillFile.write('\tidx = candidateMap["'+brname+'"] ; \n')
-    fillFile.write('\tstorageArray[idx] = ntupleRawTree.' + brname + '[candIdx] ; \n')
-  #  fillFile.write('\tstd::cout<< "'+brname+'"<<" idx : "<<idx<<"  val = "<<storageArray[idx]<<"\\n";\n\n')
-
-setupFile.write("\t return idx;")
+nCands="nDiMuCandidates"
+candMax="size"
+writeSetupAssignFiles(txt,setupFile,fillFile,nCands,candMax)
 fillFile.close()
 setupFile.close()
 
-f=open("branches.br.sc")
+f=open("branches/branches.br.sc")
 txt=f.readlines()
 f.close()
-
 setupFile=open("setupSc.cc",'w')
 fillFile =open("assignSc.cc",'w')
-
-i=0
-setupFile.write("\n")
-setupFile.write("\tauto idx = offset ;\n ")
-fillFile.write( "\tInt_t idx = 0 ;\n ")
-
-for l in txt:
-    brname=l[:-1]
-    setupFile.write("\n")
-    setupFile.write('\tcandidateMap["'+brname+'"] = idx; \n')
-    setupFile.write('\toutTree->Branch("'+brname+'" , &(storageArray[idx] ) ); \n')
-    #setupFile.write('\tstd::cout<< "'+brname+'"<<" idx : "<<idx<<"  candidateMap['+brname+'] = "<<candidateMap["'+brname+'"]<<"\\n";\n')
-    setupFile.write('\tidx++; \n')
-
-    fillFile.write('\tidx = candidateMap["'+brname+'"] ; \n')
-    fillFile.write('\tstorageArray[idx] = ntupleRawTree.' + brname + '[candIdx] ; \n')
-    #fillFile.write('\tstd::cout<< "'+brname+'"<<" idx : "<<idx<<"  val = "<<storageArray[idx]<<"\\n";\n')
-
-setupFile.write("\n\treturn idx;")
+nCands="nSCPhotons"
+candMax="size"
+writeSetupAssignFiles(txt,setupFile,fillFile,nCands,candMax)
 fillFile.close()
 setupFile.close()
+
+
+f=open("branches/branches.br.muon")
+txt=f.readlines()
+f.close()
+setupFile=open("setupMuon.cc",'w')
+fillFile =open("assignMuon.cc",'w')
+nCands="nMuons"
+candMax="size"
+writeSetupAssignFiles(txt,setupFile,fillFile,nCands,candMax)
+fillFile.close()
+setupFile.close()
+
 
--- Analysis/selections.h
+++
@@ -1,3 +0,0 @@
-#define minMuMuDr    1.4
-#define PhoDimuDrMax 1.4
-#define PhoPtMin     3.9
