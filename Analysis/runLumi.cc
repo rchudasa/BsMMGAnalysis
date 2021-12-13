9c44b46a52a6a41f8643ce11b09979d7512f6a03
aravind <aravindsugunan@gmail.com>
2021-12-13 14:18:32 +0530

    Changes to be committed: 	modified:   RunLumiSelector.h 	modified:   runLumi.cc

--- Analysis/runLumi.cc
+++ Analysis/runLumi.cc
@@ -13,6 +13,8 @@ int  main(int argc,char *argv[])
    TString fileName("certification/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.root");
    RunLumiSelector goldenJsonSelctor;
    goldenJsonSelctor.loadRunLumiMask(fileName,"RunLumiMask");
+   std::cout<<"\n";
+   goldenJsonSelctor.printAllActiveLumis();
     
   Int_t run,lumi;
 
@@ -27,6 +29,16 @@ int  main(int argc,char *argv[])
   "315363": [[1, 35], [37, 47], [49, 67], [69, 80], [82, 90]],
   "315366": [[10, 61], [67, 750]],
   "315420": [[28, 920], [924, 942], [954, 1748]],
+1 , 10 truth :  0  :  checkRunLumi  = 0
+315265 , 4 truth :  1  :  checkRunLumi  = 1
+315265 , 20 truth :  1  :  checkRunLumi  = 1
+315265 , 58 truth :  1  :  checkRunLumi  = 1
+315265 , 60 truth :  0  :  checkRunLumi  = 0
+315363 , 1 truth :  1  :  checkRunLumi  = 0
+315363 , 36 truth :  0  :  checkRunLumi  = 1
+315363 , 48 truth :  0  :  checkRunLumi  = 1
+315363 , 70 truth :  1  :  checkRunLumi  = 1
+315363 , 90 truth :  1  :  checkRunLumi  = 1
 */
 
   run=1; lumi=10;
