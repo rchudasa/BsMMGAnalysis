2b2da34a0070a0dcc25702726b1243399bb69358
aravind <aravindsugunan@gmail.com>
2022-06-14 19:44:20 +0530

    Adding the BmmMuonMVA to the workflow 	new file:   interface/XGBooster.h 	new file:   plugins/BmmMuonIdProducer.cc 	modified:   plugins/BsToMuMuGammaNTuplizer.cc 	modified:   plugins/BsToMuMuGammaNTuplizer.h 	modified:   plugins/BuildFile.xml 	new file:   plugins/XGBooster.cc 	new file:   python/BmmMuonID_cff.py 	modified:   python/Ntuplizer_cff.py 	modified:   test/devRun.py
    Adding the sample files for condor setups
        new file:   test/condor/cmsConfigs/mcUL2018_noHLT_v1.py
    	new file:   test/condor/cmsConfigs/mcUL2018_v1.py
    	new file:   test/condor/misc/makeJob.sh

---
+++ BsToMuMuGammaNTuplizer/plugins/XGBooster.cc
@@ -0,0 +1,43 @@
+#include "BsMMGAnalysis/BsToMuMuGammaNTuplizer/interface/XGBooster.h"
+#include <assert.h>
+#include <math.h>
+
+XGBooster::XGBooster(std::string model_file)
+{
+  int status = XGBoosterCreate(NULL, 0, &booster_);
+  assert(status==0 && "Failed to create XGBooster");
+  status = XGBoosterLoadModel(booster_, model_file.c_str());
+  assert(status==0 && "Failed to load XGBoost model");
+  XGBoosterSetParam(booster_, "nthread", "1");
+};
+
+void XGBooster::addFeature(std::string name){
+  features_.push_back(0);
+  feature_name_to_index_[name] = features_.size()-1;
+}
+
+void XGBooster::set(std::string name, float value){
+  if (isnan(value))
+    throw std::runtime_error(" NaN is used as value for " + name);
+  features_.at(feature_name_to_index_[name]) = value;
+}
+
+float XGBooster::predict()
+{
+  DMatrixHandle dvalues;
+  XGDMatrixCreateFromMat(&features_[0], 1, features_.size(), 0., &dvalues);
+    
+  bst_ulong out_len=0;
+  const float* score;
+
+  auto ret = XGBoosterPredict(booster_, dvalues, 0, 0, &out_len, &score);
+
+  XGDMatrixFree(dvalues);
+
+  if (ret==0) {
+    assert(out_len==1 && "Unexpected prediction format");
+    return score[0];
+  }
+    
+  return -999;    
+}
